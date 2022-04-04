d0ab12f3228fd9668aca6df092702358906b1313
Anvesh Salveru <asalveru@nvidia.com>
2022-04-04 09:09:01 +0000

    tegra_bootloader_debug: api to add profiling point
    This change exports an api to add a new profiling
    record in bootloader profiler memory.
    
    Bug 3512531
    
    Change-Id: I60fe25ab33bf607632c99a15acc24647956d4138
    Signed-off-by: Anvesh Salveru <asalveru@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2692037
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2864626
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/platform/tegra/tegra_bootloader_debug.h
+++ drivers/platform/tegra/tegra_bootloader_debug.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+// Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
 
 #ifndef __TEGRA_BOOTLOADER_DEBUG_H
 #define __TEGRA_BOOTLOADER_DEBUG_H
@@ -9,4 +9,7 @@ extern phys_addr_t tegra_bl_prof_start;
 extern phys_addr_t tegra_bl_prof_size;
 extern phys_addr_t tegra_bl_bcp_start;
 extern phys_addr_t tegra_bl_bcp_size;
+
+size_t tegra_bl_add_profiler_entry(const char *buf, size_t len);
+
 #endif
