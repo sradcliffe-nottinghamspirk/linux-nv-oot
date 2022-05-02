b41ef0cf5ebdf863d8b1ac960a6bf71a08349aa0
Rahul Bedarkar <rabedarkar@nvidia.com>
2022-05-02 11:09:05 +0000

    drivers/platform/tegra: add tegra-fsicom driver
    Add tegra-fsicom driver. It provides interface to communicate with
    Functional Safety Island (FSI).
    
    Bug 3583609
    
    Change-Id: I0cc45de69ca73092e13ee17fec4abd3f9c9e0468
    Signed-off-by: Rahul Bedarkar <rabedarkar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2707054
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    GVS: Gerrit_Virtual_Submit

---
+++ include/uapi/linux/tegra-fsicom.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: (GPL-2.0 WITH Linux-syscall-note)
+ *
+ * Copyright (c) 2021-2022, NVIDIA CORPORATION, All rights reserved.
+ */
+
+#ifndef _UAPI_TEGRA_FSICOM_H_
+#define _UAPI_TEGRA_FSICOM_H_
+
+#include <linux/ioctl.h>
+
+struct rw_data {
+	uint32_t handle;
+	uint64_t pa;
+	uint64_t iova;
+};
+
+/* signal value */
+#define SIG_FSI_DAEMON 44
+
+/* ioctl call macros */
+#define NVMAP_SMMU_MAP    _IOWR('q', 1, struct rw_data *)
+#define NVMAP_SMMU_UNMAP  _IOWR('q', 2, struct rw_data *)
+#define TEGRA_HSP_WRITE   _IOWR('q', 3, struct rw_data *)
+#define TEGRA_SIGNAL_REG  _IOWR('q', 4, struct rw_data *)
+
+#endif	/* _UAPI_TEGRA_FSICOM_H_ */
