eda8327741cc8ba34beba10f9e03601bea62468a
Bharat Nihalani <bnihalani@nvidia.com>
2022-05-04 11:31:55 +0530

    include: Fix cvnas build errors
    Fix cvnas build errors when CONFIG_TEGRA_CVNAS is disabled.
    
    The following build errors are fixed:
    
    nvhost_buffer.c:134: undefined reference to `nvcvnas_get_cvsram_base'
    nvhost_buffer.c:135: undefined reference to `nvcvnas_get_cvsram_size'
    
    Bug 3633940
    
    Change-Id: I3f42bb9a0f5d833a70fd05c494fdbbcaab60be1e
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2706940
    Reviewed-by: Ketan Patil <ketanp@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- include/linux/cvnas.h
+++ include/linux/cvnas.h
@@ -22,6 +22,7 @@
 
 #include <linux/types.h>
 
+#if defined(CONFIG_TEGRA_CVNAS) || defined(CVNAS_MODULE)
 int nvcvnas_busy(void);
 int nvcvnas_busy_no_rpm(void);
 int nvcvnas_idle(void);
@@ -30,5 +31,15 @@ int is_nvcvnas_probed(void);
 phys_addr_t nvcvnas_get_cvsram_base(void);
 size_t nvcvnas_get_cvsram_size(void);
 int is_nvcvnas_clk_enabled(void);
+#else
+static inline int nvcvnas_busy(void) { return 0; }
+static inline int nvcvnas_busy_no_rpm(void) { return 0; }
+static inline int nvcvnas_idle(void) { return 0; }
+static inline int nvcvnas_idle_no_rpm(void) { return 0; }
+static inline int is_nvcvnas_probed(void) { return 0; }
+static inline phys_addr_t nvcvnas_get_cvsram_base(void) { return 0; }
+static inline size_t nvcvnas_get_cvsram_size(void) { return 0; }
+static inline int is_nvcvnas_clk_enabled(void) { return 0; }
+#endif /* CONFIG_TEGRA_CVNAS || CVNAS_MODULE */
 
-#endif
+#endif /* __LINUX_CVNAS_H */
