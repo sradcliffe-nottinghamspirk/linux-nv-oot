7c0056394427df1e65e1431af4596ca74e626d37
Laxman Dewangan <ldewangan@nvidia.com>
2022-05-06 05:46:32 +0000

    platform/tegra: Add bootloader debug driver
    Add bootloader debug driver which provides debug functionality
    from user space.
    
    bug 3583607
    
    Change-Id: I8bdd2712e755b370b3e157bcdcd3329122729fea
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2708549
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ drivers/platform/tegra/tegra_bootloader_debug.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+
+#ifndef __TEGRA_BOOTLOADER_DEBUG_H
+#define __TEGRA_BOOTLOADER_DEBUG_H
+extern phys_addr_t tegra_bl_debug_data_start;
+extern phys_addr_t tegra_bl_debug_data_size;
+extern phys_addr_t tegra_bl_prof_start;
+extern phys_addr_t tegra_bl_prof_size;
+extern phys_addr_t tegra_bl_bcp_start;
+extern phys_addr_t tegra_bl_bcp_size;
+#endif
