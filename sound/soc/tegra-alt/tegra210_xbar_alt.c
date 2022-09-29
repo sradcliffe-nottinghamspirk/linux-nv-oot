OE4T/linux-tegra-4.9

ASoC: tegra-alt: changes needed for kernel 4.9
This patch fixes compile errors for kernel 4.9. Kernel 4.9 has two
changes, struct snd_soc_codec_driver is modified and changed PCM
runtime array to a list from struct snd_soc_card.

Bug 1856400

Change-Id: I32d0d6d5d2cb79a3af27b81786d4e288d1be4d2d
Signed-off-by: Ravindra Lokhande <rlokhande@nvidia.com>

sound/soc/tegra-alt/tegra210_xbar_alt.c
@@ -1,7 +1,7 @@
 /*
  * tegra210_xbar_alt.c - Tegra210 XBAR driver
  *
- * Copyright (c) 2014-2016 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2017 NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -29,6 +29,7 @@
 #include <linux/tegra-soc.h>
 #include <sound/soc.h>
 #include <linux/clk/tegra.h>
+#include <linux/version.h>
 
 #include "tegra210_xbar_alt.h"
 /* TODO: remove DRV_NAME_T18X after registering properly */
@@ -755,11 +756,13 @@ static const struct snd_soc_dapm_route tegra210_xbar_routes[] = {
 #if defined(CONFIG_ARCH_TEGRA_210_SOC)
 static struct snd_soc_codec_driver tegra210_xbar_codec = {
 	.probe = tegra210_xbar_codec_probe,
-	.dapm_widgets = tegra210_xbar_widgets,
-	.dapm_routes = tegra210_xbar_routes,
-	.num_dapm_widgets = ARRAY_SIZE(tegra210_xbar_widgets),
-	.num_dapm_routes = ARRAY_SIZE(tegra210_xbar_routes),
 	.idle_bias_off = 1,
+	.component_driver = {
+		.dapm_widgets = tegra210_xbar_widgets,
+		.dapm_routes = tegra210_xbar_routes,
+		.num_dapm_widgets = ARRAY_SIZE(tegra210_xbar_widgets),
+		.num_dapm_routes = ARRAY_SIZE(tegra210_xbar_routes),
+	},
 };
 #endif
 static const struct tegra210_xbar_soc_data soc_data_tegra210 = {
@@ -834,12 +837,17 @@ EXPORT_SYMBOL_GPL(tegra210_xbar_set_clock);
 static int tegra210_xbar_registration(struct platform_device *pdev)
 {
 	int ret;
+	int num_dapm_widgets, num_dapm_routes;
 
-	tegra210_xbar_codec.num_dapm_widgets = (NUM_MUX_WIDGETS * 3) +
+	num_dapm_widgets = (NUM_MUX_WIDGETS * 3) +
 				(NUM_DAIS - NUM_MUX_WIDGETS) * 2;
-	tegra210_xbar_codec.num_dapm_routes =
-		(NUM_DAIS - NUM_MUX_WIDGETS) * 2 +
-		(NUM_MUX_WIDGETS * NUM_MUX_INPUT);
+	num_dapm_routes = (NUM_DAIS - NUM_MUX_WIDGETS) * 2 +
+				(NUM_MUX_WIDGETS * NUM_MUX_INPUT);
+
+	tegra210_xbar_codec.component_driver.num_dapm_widgets =
+			num_dapm_widgets;
+	tegra210_xbar_codec.component_driver.num_dapm_routes =
+			num_dapm_routes;
 
 	ret = snd_soc_register_codec(&pdev->dev, &tegra210_xbar_codec,
 				tegra210_xbar_dais, NUM_DAIS);

sradcliffe-nottinghamspirk/linux-nv-oot

Signed-off-by: Sameer Pujar <spujar@nvidia.com>
(cherry picked from commit 9528e52063467df94bc37fe2ad10d74a1c573848)
Reviewed-on: https://git-master.nvidia.com/r/1533079
GVS: Gerrit_Virtual_Submit
Reviewed-by: Sachin Nikam <snikam@nvidia.com>

sound/soc/tegra-alt/tegra210_xbar_alt.c
@@ -531,11 +531,13 @@ static struct of_dev_auxdata tegra210_xbar_auxdata[] = {
 
 static struct snd_soc_codec_driver tegra210_xbar_codec = {
 	.probe = tegra_xbar_codec_probe,
-	.dapm_widgets = tegra210_xbar_widgets,
-	.dapm_routes = tegra210_xbar_routes,
-	.num_dapm_widgets = ARRAY_SIZE(tegra210_xbar_widgets),
-	.num_dapm_routes = ARRAY_SIZE(tegra210_xbar_routes),
 	.idle_bias_off = 1,
+	.component_driver = {
+		.dapm_widgets = tegra210_xbar_widgets,
+		.dapm_routes = tegra210_xbar_routes,
+		.num_dapm_widgets = ARRAY_SIZE(tegra210_xbar_widgets),
+		.num_dapm_routes = ARRAY_SIZE(tegra210_xbar_routes),
+	},
 };
 
 static const struct regmap_config tegra210_xbar_regmap_config = {
@@ -550,12 +552,18 @@ static const struct regmap_config tegra210_xbar_regmap_config = {
 static int tegra210_xbar_registration(struct platform_device *pdev)
 {
 	int ret;
+	int num_dapm_widgets, num_dapm_routes;
 
-	tegra210_xbar_codec.num_dapm_widgets = (TEGRA210_NUM_MUX_WIDGETS * 3) +
-				(TEGRA210_NUM_DAIS - TEGRA210_NUM_MUX_WIDGETS) * 2;
-	tegra210_xbar_codec.num_dapm_routes =
+	num_dapm_widgets = (TEGRA210_NUM_MUX_WIDGETS * 3) +
+		(TEGRA210_NUM_DAIS - TEGRA210_NUM_MUX_WIDGETS) * 2;
+	num_dapm_routes =
 		(TEGRA210_NUM_DAIS - TEGRA210_NUM_MUX_WIDGETS) * 2 +
 		(TEGRA210_NUM_MUX_WIDGETS * TEGRA210_NUM_MUX_INPUT);
+
+	tegra210_xbar_codec.component_driver.num_dapm_widgets =
+			num_dapm_widgets;
+	tegra210_xbar_codec.component_driver.num_dapm_routes =
+			num_dapm_routes;
 
 	ret = snd_soc_register_codec(&pdev->dev, &tegra210_xbar_codec,
 				tegra210_xbar_dais, TEGRA210_NUM_DAIS);