3d26b00c171a169a269579aae0b55032c7b8ee76
Arnd Bergmann <arnd@arndb.de>
2023-01-27 23:14:00 +0100

    UPSTREAM: gpu: host1x: Fix uninitialized variable use
    The error handling for platform_get_irq() failing no longer works after
    a recent change, clang now points this out with a warning:
    
    drivers/gpu/host1x/dev.c:520:6: error: variable 'syncpt_irq' is uninitialized when used here [-Werror,-Wuninitialized]
            if (syncpt_irq < 0)
                ^~~~~~~~~~
    
    Fix this by removing the variable and checking the correct error status.
    
    Fixes: 625d4ffb438c ("gpu: host1x: Rewrite syncpoint interrupt handling")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    Reported-by: "kernelci.org bot" <bot@kernelci.org>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Mikko Perttunen <mperttunen@nvidia.com>
    Change-Id: I780c935101b8c65070eeba3552f96a1bfb109592
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2853776
    Reviewed-by: Santosh BS <santoshb@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/gpu/host1x/dev.c
+++ drivers/gpu/host1x/dev.c
@@ -749,7 +749,6 @@ static int host1x_get_syncpt_pools(struct host1x *host)
 static int host1x_probe(struct platform_device *pdev)
 {
 	struct host1x *host;
-	int syncpt_irq;
 	int err;
 
 	host = devm_kzalloc(&pdev->dev, sizeof(*host), GFP_KERNEL);
@@ -784,8 +783,8 @@ static int host1x_probe(struct platform_device *pdev)
 	}
 
 	host->syncpt_irq = platform_get_irq(pdev, 0);
-	if (syncpt_irq < 0)
-		return syncpt_irq;
+	if (host->syncpt_irq < 0)
+		return host->syncpt_irq;
 
 	mutex_init(&host->devices_lock);
 	INIT_LIST_HEAD(&host->devices);
