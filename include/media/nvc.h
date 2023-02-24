ff01ad1318d5e4dcac89901ea99f762b591f2cd6
Ankur Pawar <ankurp@nvidia.com>
2023-02-24 05:47:55 +0000

    media: add imx219 sensor driver
    Add imx219 camera sensor driver code,
    mode tables and makefile changes.
    
    Bug 3583587
    
    Change-Id: Ic7693de3cbdeeaecd7d9118a1e7819eaab5fdb3a
    Signed-off-by: Ankur Pawar <ankurp@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2862964
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/media/nvc.h
+++ include/media/nvc.h
@@ -1,7 +1,6 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2012-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- *
+ * Copyright (c) 2012-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  */
 
 #ifndef __NVC_H__
