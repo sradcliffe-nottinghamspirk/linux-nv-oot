9b2d8fda4cb9e27c079550449788623cb5b5be55
Jon Hunter <jonathanh@nvidia.com>
2023-04-06 14:15:41 +0100

    nvdla: Remove dependency on CVNAS
    Tegra234 does not support CVSRAM and therefore does not need the CVNAS
    driver. When building the DLA driver as an out-of-tree module, we only
    need to support Tegra234 and so we can compile out support for CVNAS and
    remove the dependency on CVNAS. Note that CONFIG_TEGRA_CVNAS is not set
    when building out-of-tree modules.
    
    Bug 4037930
    
    Change-Id: Iabbf97b7102d8aab149baac5b0e1b044ff5e2c63
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2892313
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
    Reviewed-by: Mitch Harwell <mharwell@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/video/tegra/host/nvdla/nvdla_buffer.c
+++ drivers/video/tegra/host/nvdla/nvdla_buffer.c
@@ -11,7 +11,9 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/dma-buf.h>
+#ifdef CONFIG_TEGRA_CVNAS
 #include <linux/cvnas.h>
+#endif
 
 #include "nvdla_buffer.h"
 
@@ -106,9 +108,10 @@ static int nvdla_buffer_map(struct platform_device *pdev,
 				struct nvdla_mem_share_handle *desc,
 				struct nvdla_vm_buffer *vm)
 {
-
+#ifdef CONFIG_TEGRA_CVNAS
 	const dma_addr_t cvnas_begin = nvcvnas_get_cvsram_base();
 	const dma_addr_t cvnas_end = cvnas_begin + nvcvnas_get_cvsram_size();
+#endif
 	struct dma_buf_attachment *attach;
 	struct dma_buf *dmabuf;
 	struct sg_table *sgt;
@@ -150,6 +153,7 @@ static int nvdla_buffer_map(struct platform_device *pdev,
 	phys_addr = sg_phys(sgt->sgl);
 	dma_addr = sg_dma_address(sgt->sgl);
 
+#ifdef CONFIG_TEGRA_CVNAS
 	/* Determine the heap */
 	if (phys_addr >= cvnas_begin && phys_addr < cvnas_end)
 		vm->heap = NVDLA_BUFFERS_HEAP_CVNAS;
@@ -162,6 +166,15 @@ static int nvdla_buffer_map(struct platform_device *pdev,
 	 */
 	if (!dma_addr || vm->heap == NVDLA_BUFFERS_HEAP_CVNAS)
 		dma_addr = phys_addr;
+#else
+	vm->heap = NVDLA_BUFFERS_HEAP_DRAM;
+
+	/*
+	 * If dma address is not available, use the physical address.
+	 */
+	if (!dma_addr)
+		dma_addr = phys_addr;
+#endif
 
 	vm->sgt = sgt;
 	vm->handle = desc->share_id;
