67d0b7dd968f0955a217947c87ed655c615ad622
Laxman Dewangan <ldewangan@nvidia.com>
2023-05-13 13:51:53 +0000

    pva: Drop license paragraphs
    The pva files are having the SPDX license forma. Hence,
    it is not required to have the license paragraphs.
    
    Remove the non-required license paragraphs details.
    
    Bug 4038415
    
    Change-Id: I4d039b7d8d9ef6eea5ba53ead8c5d1e3bda223b9
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2903864
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/video/tegra/host/pva/pva.h
+++ drivers/video/tegra/host/pva/pva.h
@@ -1,21 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * drivers/video/tegra/host/pva/pva.h
+ * Copyright (c) 2016-2023, NVIDIA CORPORATION. All rights reserved.
  *
  * Tegra PVA header
- *
- * Copyright (c) 2016-2023, NVIDIA Corporation.  All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __NVHOST_PVA_H__
