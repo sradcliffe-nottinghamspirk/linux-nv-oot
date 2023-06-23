ad2857ccaa5526a58d35cf4ad8c6869df4e4ca47
Jon Hunter <jonathanh@nvidia.com>
2023-06-23 13:16:16 +0100

    soc/tegra: Clean-up fuse-helper
    The option CONFIG_TEGRA_FUSE_UPSTREAM is not needed any longer because
    we always want to enable this for Linux v5.15. Therefore, remove this
    option completely.
    
    The function tegra_fuse_control_read() is not used anywhere and so we
    can also remove this too.
    
    Bug 3777983
    
    Change-Id: I37888bc1f615f72b245c027e5c1a9251aca08af4
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/2925233
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 
-# Enable upstream fuse helper functions
-ccflags-y += -DCONFIG_TEGRA_FUSE_UPSTREAM
 LINUXINCLUDE += -I$(srctree.nvidia-oot)/include
 
 obj-m += drivers/
--- include/soc/tegra/fuse-helper.h
+++ include/soc/tegra/fuse-helper.h
@@ -8,7 +8,6 @@
 #include <soc/tegra/fuse.h>
 
 #define FUSE_SKU_INFO	0x10
-#define FUSE_BEGIN	0x100
 
 static inline u32 tegra_get_sku_id(void)
 {
@@ -17,8 +16,6 @@ static inline u32 tegra_get_sku_id(void)
 
 	return tegra_sku_info.sku_id;
 }
-
-#ifdef CONFIG_TEGRA_FUSE_UPSTREAM
 
 static inline bool tegra_platform_is_silicon(void)
 {
@@ -63,33 +60,3 @@ static inline bool tegra_platform_is_vdk(void)
 
 	return false;
 }
-
-/*
- * tegra_fuse_control_read() - allows reading fuse offsets < 0x100.
- * @offset: Offset to be read.
- * @value: Pointer to an unsigned integer where the value is stored.
- *
- * Function tegra_fuse_control_read() allows reading fuse_offsets < 0x100
- * by using the already upstreamed tegra_fuse_readl() function.
- *
- * Return: Returns a negative integer in case of error, 0 in case
- * of success.
- */
-static inline int tegra_fuse_control_read(unsigned long offset, u32 *value)
-{
-	/*
-	 * Allow reading offsets between 0x0 - 0xff.
-	 * For offsets > 0xff, use tegra_fuse_readl instead.
-	 */
-	if (WARN_ON(offset > 0xff))
-		return -EINVAL;
-
-	/*
-	 * This will overflow the offset value, which is safe as
-	 * tegra_fuse_readl() would again add 0x100 to it.
-	 */
-	offset -= FUSE_BEGIN;
-
-	return tegra_fuse_readl(offset, value);
-}
-#endif /* CONFIG_TEGRA_FUSE_UPSTREAM */
